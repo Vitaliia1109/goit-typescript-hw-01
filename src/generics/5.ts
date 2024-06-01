ce752763acf4dbda356c6f1494fc438404bd4d27
Vitaliia1109 <vitusechka@ukr.net>
2024-06-02 00:12:48 +0100

    try-1

---
+++ src/generics/5.ts
@@ -0,0 +1,17 @@
+export enum UserRole {
+  admin = "admin",
+  editor = "editor",
+  guest = "guest",
+}
+
+// Замініть наступний код на версію за допомогою Record
+
+type UserRolesStatuses = Record<UserRole, string>;
+
+const RoleDescription: UserRolesStatuses = {
+  [UserRole.admin]: "Admin User",
+  [UserRole.editor]: "Editor User",
+  [UserRole.guest]: "Guest User",
+};
+
+console.log(RoleDescription[UserRole.admin]);
